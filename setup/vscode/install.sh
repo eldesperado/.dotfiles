e5d30477867bc9546e425678b95a31e00267aa60
Pham Nguyen Nhat Trung <trungpnn@vng.com.vn>
2018-05-29 00:28:44 +0700

    Add vscode script & settings.json
    Pulled By: trungpnn

---
+++ setup/vscode/install.sh
@@ -0,0 +1,4 @@
+code --install-extension WakaTime.vscode-wakatime
+code --install-extension alexdima.copy-relative-path
+code --install-extension xaver.clang-format
+code --install-extension zhuangtongfa.material-theme
