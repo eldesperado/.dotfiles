4037aecf3080729b0803d090394919b0967ddf6f
TrungPNN <trungpnn@vng.com.vn>
2019-05-16 12:55:08 +0700

    [Utils] Move methods to prompt.sh file
    Pulled By: trungpnn

--- setup/lib/prompt.sh
+++ setup/lib/prompt.sh
@@ -1,8 +1,25 @@
 #!/usr/local/env bash
 
-lib_dir="$(dirname "$(realpath "$0")")"
-source "$lib_dir/../lib/log.sh"
+# Logging functions
+info () {
+  printf "  [ \033[00;34m..\033[0m ] %s\n" "$1"
+}
 
+user () {
+  printf "\r  [ \033[0;33m?\033[0m ] %s" "$1"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] %s\n" "$1"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] %s\n" "$1"
+  echo
+  exit
+}
+
+# Prompt
 yes_no_prompt () {
   local result
   while true; do
