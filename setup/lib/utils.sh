60e048f64d3abfda3c89b0ea712f1400e00c0e24
TrungPNN <trungpnn@vng.com.vn>
2019-05-16 12:41:34 +0700

    [Utils] Implement 'realpath' alternative method
    Pulled By: trungpnn

---
+++ setup/lib/utils.sh
@@ -0,0 +1,12 @@
+realpath() {
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+}
